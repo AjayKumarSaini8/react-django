a360225249f3e4f7c7a9abf9a448e55ae9996771
AjayKumarSaini8 <108985901+AjayKumarSaini8@users.noreply.github.com>
2024-03-07 22:43:40 +0530

    first

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,10 @@
-import axios from 'axios';
-import React from 'react';
-
+import axios from 'axios'
+import React from 'react'
 class App extends React.Component {
 
   state = { details: [], }
 
   componentDidMount() {
-
     let data;
     axios.get('http://127.0.0.1:8000').then(res => {
       data = res.data;
